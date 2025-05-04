bb61c6f125e4c0bfed14b680975fd960d5b635bc
motiurrahamanfahim@gmail.com <motiurrahamanfahim@gmail.com>
2025-05-04 15:13:43 +0600

    modified:   config/config.properties 	modified:   locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,26 +3,21 @@ from tests.demo_test import UserAPITest
 from helper.config_reader import load_config
 
 config = load_config()
+base_url = config.get("base_url")
+min_wait = int(config.get("minWaitTime"))
+max_wait = int(config.get("maxWaitTime"))
 
-# Fetch configuration values directly from the loaded config
-base_url = config.get("baseUrl")
-wait_time = int(config.get("waitTime", 1))
-spawn_rate = int(config.get("spawnRate", 2))
-users = int(config.get("users", 10))  # Default to 10 users
-run_time = config.get("runTime", "1m")
-headless = config.get("headless", "true").lower() == "true"
-
-class UserAPITest(HttpUser):
-    wait_time = between(wait_time, wait_time)
-    host = base_url
+class LocustFile(HttpUser):
+    wait_time = between(min_wait, max_wait)
+    host = config.get(base_url)
 
     def on_start(self):
         """Setup logic for when a user starts (e.g., login)"""
-        pass
+        self.test_api = UserAPITest()
 
     @task
     def get_user(self):
-        self.test_api.test_get_user()  # This lives in test_user_api.py
+        self.test_api.test_get_user()
 
 
 
