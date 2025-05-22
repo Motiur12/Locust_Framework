5b344c45a70b93afad0c4933f73aa5abdbafc602
motiurrahamanfahim@gmail.com <motiurrahamanfahim@gmail.com>
2025-05-22 12:48:13 +0600

    Changes to be committed: 	modified:   locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,6 @@ from pages.list import ListPage
 from helper.logging_setup import setup_file_logging
 
 setup_file_logging()
-
 from locust import HttpUser, task, between
 from locust.stats import stats_printer, stats_history
 from locust import events
