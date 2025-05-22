dd43fe7f6775959680bb1680bb25dce35376ef15
motiurrahamanfahim@gmail.com <motiurrahamanfahim@gmail.com>
2025-05-22 12:47:09 +0600

    Changes to be committed: 	new file:   Dockerfile.locust 	modified:   basepage/base_page.py 	renamed:    locust.py -> locustfile.py 	new file:   prometheus/prometheus.yml 	new file:   requirements.t 	new file:   requirements.txt 	new file:   results_exceptions.csv 	new file:   results_failures.csv 	new file:   results_stats.csv 	new file:   results_stats_history.csv

--- basepage/base_page.py
+++ basepage/base_page.py
@@ -1,21 +1,18 @@
 import time
 import json
 import logging
-from locust import HttpUser
-from requests.models import Response
 from helper.prepare_headers import prepare_headers
 from helper.prepare_params import prepare_params
 from helper.log_and_time_request import log_and_time_request
 
-
 class BasePage:
-    def __init__(self, client: HttpUser, bearer_token: str = None, console_logging: bool = False):
+    def __init__(self, client, bearer_token: str = None, console_logging: bool = False):
         self.client = client
         self.bearer_token = bearer_token
         self.console_logging = console_logging
 
     ##============================GET============================##
-    def get(self, endpoint: str, headers: dict = None, params: dict = None) -> Response:
+    def get(self, endpoint: str, headers: dict = None, params: dict = None):
             headers = prepare_headers(headers, self.bearer_token)
             params = prepare_params(params)
             start = time.time()
@@ -24,18 +21,18 @@ class BasePage:
             return response
     
     ##============================POST============================##
-    def post(self, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None) -> Response:
+    def post(self, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None):
         return self._send_with_body("POST", endpoint, data, json_data, headers)
     
-    def post_multipart(self, endpoint: str, data: dict = None, files: dict = None, headers: dict = None) -> Response:
+    def post_multipart(self, endpoint: str, data: dict = None, files: dict = None, headers: dict = None):
         return self._send_multipart("POST", endpoint, data, files, headers)
     
     ##============================PUT============================##
-    def put(self, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None) -> Response:
+    def put(self, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None):
         return self._send_with_body("PUT", endpoint, data, json_data, headers)
     
     ##============================DELETE============================##
-    def delete(self, endpoint: str, headers: dict = None) -> Response:
+    def delete(self, endpoint: str, headers: dict = None):
         headers = prepare_headers(headers)
         start = time.time()
         response = self.client.delete(endpoint, headers=headers)
@@ -44,7 +41,7 @@ class BasePage:
     
     
     ##=======================================================================================================##
-    def _send_with_body(self, method: str, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None) -> Response:
+    def _send_with_body(self, method: str, endpoint: str, data: dict = None, json_data: dict = None, headers: dict = None):
         self._check_data_and_json(data, json_data)
         headers = prepare_headers(headers)
         start = time.time()
@@ -55,7 +52,7 @@ class BasePage:
         log_and_time_request(method, endpoint, response, duration=time.time() - start, console_logging=self.console_logging)
         return response
     
-    def _send_multipart(self, method: str, endpoint: str, data: dict = None, files: dict = None, headers: dict = None) -> Response:
+    def _send_multipart(self, method: str, endpoint: str, data: dict = None, files: dict = None, headers: dict = None):
         headers = prepare_headers(headers)
         start = time.time()
         method_func = getattr(self.client, method.lower())
@@ -63,13 +60,13 @@ class BasePage:
         log_and_time_request(method, endpoint, response, duration=time.time() - start, console_logging=self.console_logging)
         return response
     
-    def assert_status_code(self, response: Response, expected_code: int):
+    def assert_status_code(self, response, expected_code: int):
         if response.status_code != expected_code:
             message = f"Expected {expected_code}, but got {response.status_code}. Response body: {response.text}"
             self.log(message)
             raise AssertionError(message)
 
-    def get_json(self, response: Response):
+    def get_json(self, response):
         try:
             return response.json()
         except (ValueError, json.JSONDecodeError) as e:
--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,13 @@ from helper.logging_setup import setup_file_logging
 setup_file_logging()
 
 from locust import HttpUser, task, between
+from locust.stats import stats_printer, stats_history
+from locust import events
+from locust_plugins.prometheus import PrometheusExporter
+
+PrometheusExporter().start()
+
+
 
 class ReqresUser(HttpUser):
     wait_time = between(1, 3)
